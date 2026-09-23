353c255195335ecce81e18877d7ef119529b00c1
Natnael Birhanu <natnaelaster42@gmail.com>
2026-09-23 13:07:56 -0700

    exercise on modules

---
+++ week-01-python-oop/exercises/ex_module.py
@@ -0,0 +1,18 @@
+# math_utils.py
+PI = 3.14159
+
+def area_circle(radius):
+    return PI * radius ** 2
+
+def area_square(side):
+    return side ** 2
+
+
+# greetings.py
+DEFAULT_LANG = "en"
+
+def hello(name):
+    return f"Hello, {name}!"
+
+def goodbye(name):
+    return f"Goodbye, {name}!"
